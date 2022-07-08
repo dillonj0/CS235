93173878e82dbd0f16385a134dbc1bed43849eb1
Lee <lee.jensen@agilix.com>
2022-07-08 11:50:18 -0600

    This is probably it's final version.

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -4,42 +4,56 @@
 #include <vector>
 #include <fstream>
 #include <map>
-#include <time.h>
+#include <list>
+#include <sstream>
+//#include <time.h>
 using namespace std;
 // Created by Dillon Jensen on 06/JUL/2022
 
 void FileToSetAndVector(string fileName, vector <string> &tokens, set <string> &unique);
 void WriteSetToFile(string fileName, set <string> unique);
 void WriteVectorToFile(string fileName, vector <string> tokens);
-void VectorToMap(vector <string> tokens, map <string, vector <string> > &wordMap);
-void WriteMapToFile(string fileName, map <string, vector <string> > wordMap);
-void GeneratePredictedWords(int wordCount, map <string, vector <string> > wordMap, string startWord);
+void VectorToMap(vector <string> tokens, map <list <string>,vector <string> > &wordMap,
+                 int refCount);
+void WriteMapToFile(string fileName, map <list <string>, vector <string> > wordMap);
+void GeneratePredictedWords(int wordCount, map <list <string>, vector <string> > wordMap,
+                            string startWord);
 
 int main(int argc, char *argv[]) {
     string fileName = argv[1];
-    string predictionStartsWith;
-    int numWords;
+    string predictionStartsWith;    //The input key
+    int numWords;                   //The number of words to be output based on the
+                                    //   .txt file and the input key.
+    int refCount = 1;               //The number of words in the input key
 
     ////************** PROGRAM SETUP ****************
     cout << endl;
     if(argv[2]) {
         predictionStartsWith = argv[2];
-    } else{
+        // Also get the number of words in the input:
+        for (int i = 0; i < predictionStartsWith.size(); i++){
+            if (predictionStartsWith[i] == ' '){
+                refCount++;
+            }
+        }
+        cout << "*** Input string contains " << refCount << " words. ***" << endl;
+    } else {
         cout << "*** No startWord argument. Using text file start as default. ***" << endl;
         predictionStartsWith = "";
     }
     if(argv[2] && argv[3]) {
         numWords = stoi(argv[3]);
-    } else{
+    } else {
         cout << "*** No numWords argument. Using default of 100. ***" << endl;
         numWords = 100;
     }
+
     cout << "BEGINNING PROCESS... " << endl;
     ////************* END PROGRAM SETUP **************
 
     vector <string> tokens;     // All the words in order
     set <string> unique;        // Only keeps the new words
-    map <string, vector <string> >wordMap;
+    map <list <string>, vector <string> >wordMap;
 
 
 
@@ -48,11 +62,12 @@ int main(int argc, char *argv[]) {
     WriteSetToFile(fileName, unique);
     WriteVectorToFile(fileName, tokens);
 
-    VectorToMap(tokens, wordMap);
+    VectorToMap(tokens, wordMap, refCount);
     WriteMapToFile(fileName, wordMap);
 
     GeneratePredictedWords(numWords, wordMap, predictionStartsWith);
 
+    cout << endl << "PROCESS COMPLETE." << endl;
     return 0;
 }
 
@@ -114,50 +129,75 @@ void WriteVectorToFile(string fileName, vector <string> tokens){
     }
 }
 
-void VectorToMap(vector <string> tokens, map <string, vector <string> > &wordMap){
+void VectorToMap(vector <string> tokens, map <list <string>, vector <string> > &wordMap,
+                 int refCount){
     cout << "Writing vector to map." << endl;
     cout << "..." << endl;
 
-    string last = "";
+    list<string> state;
+    for (int i = 0; i < refCount; i++){
+        state.push_back("");
+    }
     for (int i = 0; i < tokens.size(); i++){
-        wordMap[last].push_back(tokens.at(i));
-        last = tokens.at(i);
+        wordMap[state].push_back(tokens.at(i));
+        state.push_back(tokens.at(i));
+        state.pop_front();
     }
 }
 
-void WriteMapToFile(string fileName, map <string, vector <string> > wordMap){
+void WriteMapToFile(string fileName, map <list <string>, vector <string> > wordMap){
     cout << "Writing map to file \"" << fileName << "_map.txt\"" << endl;
 //    cout << "..." << endl;
 
     string newFileName = fileName + "_map.txt";
 
     ofstream outFile(newFileName);
-    map<string, vector <string> >::iterator i;
+    map <list <string>, vector <string> >::iterator i;
     for (i = wordMap.begin(); i != wordMap.end(); i++) {
         //"key, value\n"
-        outFile << i->first << ", ";
-        for (int j = 0; j < i->second.size(); j++){
-            outFile << i->second.at(j) << ", ";
+        list <string> temp = i->first;
+        for (int j = 0; j < i->first.size(); j++){
+            outFile << temp.front() << ", ";
+            temp.pop_front();
+        }
+        outFile << "; ";
+        for (int k = 0; k < i->second.size(); k++){
+            outFile << i->second.at(k) << ", ";
         }
         outFile << endl;
     }
 }
 
-void GeneratePredictedWords(int wordCount, map <string, vector <string> > wordMap, string startWord){
+void GeneratePredictedWords(int wordCount, map <list <string>, vector <string> > wordMap,
+                            string startWord){
     cout << endl;
     cout << "Generating " << wordCount << " words:" << endl;
-    string state = startWord;
-    if (startWord != "" && wordMap.count(startWord)!= 0) {
+
+    list <string> state;
+    istringstream in(startWord);
+    string input;
+    while(in >> input){
+        state.push_back(input);
+    }
+
+    if (startWord != "" && wordMap.count(state)!= 0) {
         cout << startWord << " ";
     }
     else{
-        cout << "*** START WORD \"" << startWord << "\" NOT FOUND IN MAP. ***" << endl;
+        cout << "*** START PHRASE \"" << startWord << "\" NOT FOUND IN MAP. ***" << endl;
     }
+
     for (int i = 0; i < wordCount; i++) {
-        srand (time(NULL)); //We don't want the output to be the same every time.
-        int ind = rand() % wordMap[state].size();
-        cout << wordMap[state][ind] << " ";      /////todo problematic
-        state = wordMap[state][ind];             /////todo problematic
+//        srand (time(0)); //We don't want the output to be the same every time.
+        // ^^^^^ RANDOM JUST GETS IT STUCK IN A LOOP AGAIN SO I GUESS WE'LL SETTLE.
+//        cout << wordMap[state].size() << " <<< wordMap size at " << startWord << endl;
+        int ind = (rand() + time(NULL)) % wordMap[state].size();
+        cout << wordMap[state][ind] << " ";
+        state.push_back(wordMap[state][ind]);
+        state.pop_front();
+        if (i % 10 == 0){
+            cout << endl; //line break after each 10 words
+        }
     }
     cout << endl;
 }
